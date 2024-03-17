9c592aa4a7424df0f245262f8a0d2b705e364653
Keshab Kumar Sasmal <keshabsasmal204@gmail.com>
2024-03-18 00:59:57 +0530

    new files are below

---
+++ Area of a circle.c
@@ -0,0 +1,10 @@
+//Area of a cicle. 
+#include<stdio.h>
+int main(){
+	float radious,area;
+	printf("enter the radious of circle\n");
+	scanf("%f",&radious);
+	area=3.141*radious*radious;
+	printf("area:%f",area);
+	return 0;
+}
